5d33b06b67e0c93d1ec2eedb8bf47923a3c820ae
mostafa eldweek <mostafaeldweek98@gmail.com>
2024-02-03 04:14:33 +0200

    my assessment

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-console.log('Add your name in {{name}}');
-console.log('Add the current date in {{date}}');
-console.log('Add a dialog that triggers when the visitor clicks Contact information');
+document.addEventListener("DOMContentLoaded", function () {
+  const openModalBtn = document.getElementById("openModalBtn");
+  const modal = document.getElementById("myModal");
+  const closeModalBtn = document.getElementById("closeModalBtn");
+
+  openModalBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  closeModalBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+document.getElementById("myName").innerText = "Mostafa Eldweek";
+
+var currentDate = new Date().toLocaleDateString();
+var currentDateElement = document.getElementById("currentDate");
+currentDateElement.innerText = currentDate;
+
+var newStyles = {
+  color: "#3282b8",
+  fontSize: "18px",
+  marginBottom: "100px",
+  display: "flex",
+  justifyContent: "center",
+  fontSize: "20px",
+  fontWeight: "bold",
+};
+Object.assign(currentDateElement.style, newStyles);
